b68e3bfb02c8db19da1233e60901cc1f6d37f75c
Steve Jerett McQueen <sjmsj1321@yahoo.com>
2017-10-30 12:39:22 +0000

    modified main.cpp

--- main.cpp
+++ main.cpp
@@ -15,24 +15,37 @@
 #include "sectionize.hpp"
 
 int main(){
-
+    
     std::string input;
-    std::cout << "Please Enter The Cipher Input: ";
-    std::getline(std::cin, input);
-    std::cout << "The Ciphered Input You Entered is: ( " << input << " )" << std::endl;
-
-    // Thread ID
-    pthread_t sifter_thread;
-
-    // Thread attributes
-    pthread_attr_t sft_attr;
-    pthread_attr_init(&sft_attr);
-    
-    // Create thread
-    pthread_create(&sifter_thread, &sft_attr, sifter_runner, &input);
-    
-    // Suspend wait during execution
-    pthread_join(sifter_thread, NULL);
+    while(input != ":exit"){
+        
+        std::cout << "Please Enter The Cipher Input or Enter ':exit' To Exit Program: ";
+        std::getline(std::cin, input);
+        
+        if(input == ":exit"){
+            
+            std::cout << ">>> Program Exit <<<" << std::endl;
+            break;
+            
+        // Launch thread    
+        } else {
+            
+            // Thread ID
+            pthread_t sifter_thread;
+        
+            // Thread attributes
+            pthread_attr_t sft_attr;
+            pthread_attr_init(&sft_attr);
+            
+            // Create thread
+            pthread_create(&sifter_thread, &sft_attr, sifter_runner, &input);
+            
+            // Suspend wait during execution
+            pthread_join(sifter_thread, NULL);
+        
+        }
+        
+    } 
     
     return 0;
     
